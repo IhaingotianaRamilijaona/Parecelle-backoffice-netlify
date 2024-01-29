d70c01f11496f45a5da9335b6b7b98e542561d53
IhaingotianaRamilijaona <120670980+IhaingotianaRamilijaona@users.noreply.github.com>
2024-01-29 10:54:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          LEARN NEW THING
+          LEARN NEW THINGSSSSS
         </a>
       </header>
     </div>
