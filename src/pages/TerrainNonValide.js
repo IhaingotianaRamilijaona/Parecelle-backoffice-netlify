0a7cafdd5b6bac35bfe8a0aab43a4e965e337f20
IhaingotianaRamilijaona <120670980+IhaingotianaRamilijaona@users.noreply.github.com>
2024-01-29 18:23:59 +0100

    Restructuration

--- src/pages/TerrainNonValide.js
+++ src/pages/TerrainNonValide.js
@@ -1,6 +1,5 @@
 import React, { useState,useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import ListTerrainsNonValide  from '../components/ListTerrainsNonValide';
 import loadingSpinner from '../img/loading.gif';
 
 export default function TerrainNonValide () {
